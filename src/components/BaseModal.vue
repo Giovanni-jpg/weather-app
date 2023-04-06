2337d911c0598659fab6ec223edcdba32f85b114
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2023-04-06 12:49:58 +0200

    Adding transition style to the modal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,13 +1,19 @@
 <template>
-  <div
-    v-if="modalActive"
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-  >
-    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
-      <slot />
-      <button @click="$emit('close-modal')" class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
-    </div>
-  </div>
+  <Teleport to="body">
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition nname="modal-inner">
+          <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+            <slot />
+            <button @click="$emit('close-modal')" class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
+  </Teleport>
 </template>
 
 <script setup>
@@ -20,4 +26,26 @@ defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
